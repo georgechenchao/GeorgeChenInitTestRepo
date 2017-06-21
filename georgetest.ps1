caf9912e8b2fe78a0e7bb8136b9933c738cbb344
chaochen <chaochen@microsoft.com>
2017-06-21 16:50:58 +0800

    Updte for test 8

--- georgetest.ps1
+++ georgetest.ps1
@@ -16,67 +16,70 @@ $url = "https://github.com/mono/api-doc-tools/releases/download/preview-5.0.0.14
 $currentCommit = $env:GIT_COMMIT
 $lastCommit = $env:GIT_PREVIOUS_COMMIT
 $changeListForRepo = &git diff --name-only $lastCommit $currentCommit
-$changeListForRepoArray = $changeListForRepo.Split("`n");
-$dllFolderSet = New-Object System.Collections.Generic.List[System.Object];
-ForEach($fileItem in $changeListForRepoArray)
+if ($currentCommit -ne $lastCommit -and $changeListForRepo)
 {
-    if ($fileItem.Trim().EndsWith("dll"))
+    $changeListForRepoArray = $changeListForRepo.Split("`n");
+    $dllFolderSet = New-Object System.Collections.Generic.List[System.Object];
+    ForEach($fileItem in $changeListForRepoArray)
     {
-        $folderName = $fileItem.Split("/")[0].Trim();
-        if ($dllFolderSet -notcontains $folderName)
+        if ($fileItem.Trim().EndsWith("dll"))
         {
-            $dllFolderSet.Add($folderName);
+            $folderName = $fileItem.Split("/")[0].Trim();
+            if ($dllFolderSet -notcontains $folderName)
+            {
+                $dllFolderSet.Add($folderName);
+            }
         }
     }
-}
-$dllFolderSet;
+    $dllFolderSet;
 
-$scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
-Push-Location $scriptPath
+    $scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
+    Push-Location $scriptPath
 
-$mdocZipPath = Join-Path $scriptPath "mdoc.zip"
-$mdocPath = Join-Path $scriptPath "mdoc"
-$mdocExePath = Join-Path $mdocPath "mdoc.exe"
-if (Test-Path $mdocZipPath)
-{
-    Remove-Item $mdocZipPath
-}
-if (Test-Path $mdocPath)
-{
-    Remove-Item $mdocPath -Recurse
-}
-New-Item $mdocPath -type directory
-Write-Output "Dowloading mdoc from $url"
-Invoke-WebRequest -Uri $url -OutFile $mdocZipPath
-Unzip $mdocZipPath $mdocPath
+    $mdocZipPath = Join-Path $scriptPath "mdoc.zip"
+    $mdocPath = Join-Path $scriptPath "mdoc"
+    $mdocExePath = Join-Path $mdocPath "mdoc.exe"
+    if (Test-Path $mdocZipPath)
+    {
+        Remove-Item $mdocZipPath
+    }
+    if (Test-Path $mdocPath)
+    {
+        Remove-Item $mdocPath -Recurse
+    }
+    New-Item $mdocPath -type directory
+    Write-Output "Dowloading mdoc from $url"
+    Invoke-WebRequest -Uri $url -OutFile $mdocZipPath
+    Unzip $mdocZipPath $mdocPath
 
-$contentRepoPath = Join-Path $scriptPath "contentRepo"
-$xmlPath = Join-Path $contentRepoPath $xmlPath
-if (-Not (Test-Path $xmlPath))
-{
-    New-Item $xmlPath -ItemType directory
-}
-& git clone $contentRepoUrl $contentRepoPath
-Push-Location $contentRepoPath
-$checkBr = & git ls-remote --heads $contentRepoUrl $branch
-& git fetch
-& git checkout -B $branch
-if (-Not [string]::IsNullOrEmpty($checkBr)) {
-    & git branch --set-upstream-to=origin/$branch
-    & git pull
-}
-Pop-Location
+    $contentRepoPath = Join-Path $scriptPath "contentRepo"
+    $xmlPath = Join-Path $contentRepoPath $xmlPath
+    if (-Not (Test-Path $xmlPath))
+    {
+        New-Item $xmlPath -ItemType directory
+    }
+    & git clone $contentRepoUrl $contentRepoPath
+    Push-Location $contentRepoPath
+    $checkBr = & git ls-remote --heads $contentRepoUrl $branch
+    & git fetch
+    & git checkout -B $branch
+    if (-Not [string]::IsNullOrEmpty($checkBr)) {
+        & git branch --set-upstream-to=origin/$branch
+        & git pull
+    }
+    Pop-Location
 
-ForEach($folderName in $dllFolderSet)
-{
-    & $mdocExePath fx-bootstrap (".\" + $folderName)
-    & $mdocExePath update -o $xmlPath -fx (".\" + $folderName) -use-docid
+    ForEach($folderName in $dllFolderSet)
+    {
+        & $mdocExePath fx-bootstrap (".\" + $folderName)
+        & $mdocExePath update -o $xmlPath -fx (".\" + $folderName) -use-docid
+    }
+
+    Push-Location $contentRepoPath
+    & git add -A
+    & git commit -m "mdoc CI update"
+    & git push --set-upstream origin $branch
+    Pop-Location
+
+    Pop-Location
 }
-
-Push-Location $contentRepoPath
-& git add -A
-& git commit -m "mdoc CI update"
-& git push --set-upstream origin $branch
-Pop-Location
-
-Pop-Location
